7456ce4c0c253e71528bf6f98ab445076eef8910
romeo <rjameson1928@gmail.com>
2023-01-04 13:45:39 +0300

    hw - 1

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import s from './Post.module.css'
 
-type PropsType = {
+type MessageType = {
 	message: string
 }
 
-export const Post = (props: PropsType) => {
+export const Post = (props: MessageType) => {
 	return (
 		<div className={s.item}>
 			<img src="https://uznayvse.ru/images/catalog/2022/2/cristiano-ronaldo_56.jpg"/>
